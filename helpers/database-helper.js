31db9996a6e1382acec904b92e3094d7f42f0a16
TakundaMabukwa <mabukwatakunda@gmail.com>
2025-12-28 17:07:56 +0200

    update all fields from tcp

--- helpers/database-helper.js
+++ helpers/database-helper.js
@@ -4,15 +4,15 @@ async function updateVehicleData(trackingData) {
   try {
     // First try to find by IP address (Pocsagstr)
     let result = await pool.query(
-      'UPDATE vehicles SET speed = $1, latitude = $2, longitude = $3, loctime = $4, mileage = $5 WHERE ip_address = $6 RETURNING id',
-      [trackingData.Speed, trackingData.Latitude, trackingData.Longitude, trackingData.LocTime, trackingData.Mileage, trackingData.Pocsagstr]
+      'UPDATE vehicles SET plate = $1, speed = $2, latitude = $3, longitude = $4, loctime = $5, mileage = $6, status = $7, geozone = $8, fueldata = $9, driver_name = $10, iteminstalled = $11 WHERE ip_address = $12 RETURNING id',
+      [trackingData.Plate, trackingData.Speed, trackingData.Latitude, trackingData.Longitude, trackingData.LocTime, trackingData.Mileage, trackingData.Status, trackingData.Geozone, trackingData.FuelData, trackingData.DriverName, trackingData.ItemInstalled, trackingData.Pocsagstr]
     );
 
     // If no rows updated by IP, try by reg (Plate)
     if (result.rowCount === 0 && trackingData.Plate) {
       result = await pool.query(
-        'UPDATE vehicles SET speed = $1, latitude = $2, longitude = $3, loctime = $4, mileage = $5, ip_address = $6 WHERE reg = $7 RETURNING id',
-        [trackingData.Speed, trackingData.Latitude, trackingData.Longitude, trackingData.LocTime, trackingData.Mileage, trackingData.Pocsagstr, trackingData.Plate]
+        'UPDATE vehicles SET plate = $1, speed = $2, latitude = $3, longitude = $4, loctime = $5, mileage = $6, status = $7, geozone = $8, fueldata = $9, driver_name = $10, iteminstalled = $11, ip_address = $12 WHERE reg = $13 RETURNING id',
+        [trackingData.Plate, trackingData.Speed, trackingData.Latitude, trackingData.Longitude, trackingData.LocTime, trackingData.Mileage, trackingData.Status, trackingData.Geozone, trackingData.FuelData, trackingData.DriverName, trackingData.ItemInstalled, trackingData.Pocsagstr, trackingData.Plate]
       );
     }
 
